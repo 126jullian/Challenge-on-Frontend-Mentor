6b43b1dca75030c5ca8a30d9cf7f3ba43d7e380a
Jullian <jullian840126@gmail.com>
2022-11-20 23:26:42 +0800

    git commit -m Add hamburger animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const btnHamburger = document.querySelector(`#btnHamburger`);
+
+btnHamburger.addEventListener(`click`, function(){
+    console.log(`click hamburger`);
+
+    btnHamburger.classList.toggle('open');
+});
